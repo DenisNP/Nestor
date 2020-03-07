11acd0810768571c0feb2df1920441d03ddb78d1
Denis Peshekhonov <denis@peshekhonov.ru>
2020-03-07 19:28:16 +0300

    remove chronicles, update to .NET Core 3.1

--- Nestor.DictBuilder/Program.cs
+++ Nestor.DictBuilder/Program.cs
@@ -4,26 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            // LoadNestorDictionary();
-            LoadNestorChroniclesDictionary();
+            LoadNestorDictionary();
         }
 
         private static void LoadNestorDictionary()
         {
             new NestorLoader().BuildDictionary("hagen", "dict_new.bin");
-        }
-
-        private static void LoadNestorChroniclesDictionary()
-        {
-            const int from = 20000;
-            const int to = 30000;
-            
-            new NestorChroniclesLoader().BuildDictionary(
-                "model_large",
-                "wiki_ruscorp.bin",
-                from,
-                to
-            );
         }
     }
 }
