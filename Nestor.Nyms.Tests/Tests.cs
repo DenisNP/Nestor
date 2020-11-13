90f80c486cd3374d1d58f3af3043efb30cc99eeb
Denis Peshekhonov <denis@peshekhonov.ru>
2020-11-14 01:07:29 +0300

    fixes

--- Nestor.Nyms.Tests/Tests.cs
+++ Nestor.Nyms.Tests/Tests.cs
@@ -19,6 +19,21 @@ namespace Nestor.Nyms.Tests
             var s = _nNyms.Synonyms(word);
             
             Assert.IsTrue(s.Contains("противник"));
+
+            var areSynonyms = _nNyms.AreSynonyms("красивый", "прекрасный");
+            Assert.IsTrue(areSynonyms);
+        }
+
+        [Test]
+        public void TestAntonyms()
+        {
+            const string word = "враг";
+            var s = _nNyms.Antonyms(word);
+            
+            Assert.IsTrue(s.Contains("друг"));
+
+            var areAntonyms = _nNyms.AreAntonyms("большой", "маленький");
+            Assert.IsTrue(areAntonyms);
         }
     }
 }
